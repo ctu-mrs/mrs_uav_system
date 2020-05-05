ebb7dad5d3a9635d56a96218105f192e2b38477a
Robert Penicka <penicrob@fel.cvut.cz>
2020-05-05 16:13:58 +0200

    add deploy ket and its usage in travis

--- .ci/ci_before_install.sh
+++ .ci/ci_before_install.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Starting install preparation" 
-openssl aes-256-cbc -K $encrypted_d9693e727195_key -iv $encrypted_d9693e727195_iv -in ./.ci/deploy_key_github.enc -out ./.ci/deploy_key_github -d
+openssl aes-256-cbc -K $encrypted_f0fd3ee254e8_key -iv $encrypted_f0fd3ee254e8_iv -in ./.ci/deploy_key_github.enc -out ./.ci/deploy_key_github -d
 eval "$(ssh-agent -s)"
 chmod 600 ./.ci/deploy_key_github
 ssh-add ./.ci/deploy_key_github
