632c85e6aa0f2a388e2046f0a1a75c30e9039f20
Tomas Baca <klaxalk@gmail.com>
2020-05-05 14:45:10 +0200

    fixed student workspace setup

--- scripts/set_my_workspace.sh
+++ scripts/set_my_workspace.sh
@@ -22,19 +22,21 @@ command catkin init
 echo "$0: setting up build profiles"
 command catkin config --profile debug --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17 -march=native -fno-diagnostics-color'  -DCMAKE_C_FLAGS='-march=native -fno-diagnostics-color'
 command catkin profile set debug
-command catkin config --extend $MRS_WORKSPACE
+command catkin config --extend $MRS_WORKSPACE/devel
 
 command catkin config --profile release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17 -march=native -fno-diagnostics-color'  -DCMAKE_C_FLAGS='-march=native -fno-diagnostics-color'
 command catkin profile set release
-command catkin config --extend $MRS_WORKSPACE
+command catkin config --extend $MRS_WORKSPACE/devel
 
 command catkin config --profile reldeb --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17 -march=native -fno-diagnostics-color' -DCMAKE_C_FLAGS='-march=native -fno-diagnostics-color'
 command catkin profile set reldeb
-command catkin config --extend $MRS_WORKSPACE
+command catkin config --extend $MRS_WORKSPACE/devel
 
 echo "$0: cloning example packages"
 cd ~/git
-git clone --recursive git@github.com:ctu-mrs/example_ros_packages
+[ ! -e example_ros_packages ] && git clone git@github.com:ctu-mrs/example_ros_packages
+cd example_ros_packages
+gitman install
 
 echo "$0: linking example packages to ~/workspace"
 cd $WORKSPACE_PATH/src
