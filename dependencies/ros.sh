4859018835793cd44f7c70151c5a432d32bfeb97
Tomas Baca <klaxalk@gmail.com>
2020-05-23 23:20:32 +0200

    updated ros.sh

--- dependencies/ros.sh
+++ dependencies/ros.sh
@@ -8,7 +8,15 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 echo "$0: installing ROS"
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+for server in ha.pool.sks-keyservers.net \
+              hkp://p80.pool.sks-keyservers.net:80 \
+              keyserver.ubuntu.com \
+              hkp://keyserver.ubuntu.com:80 \
+              pgp.mit.edu; do
+    sudo apt-key adv --keyserver "$server" --recv-keys C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 && break || echo "Trying new server..."
+done
+
 sudo apt -y update
 sudo apt -y install ros-melodic-desktop-full
 
