a69b248876d39ee6bae222c6905952fa9338c4af
Tomas Baca <klaxalk@gmail.com>
2020-05-23 21:11:08 +0200

    fixed takeoff test script

--- .ci/takeoff_test.sh
+++ .ci/takeoff_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "Starting takeoff test" 
-cd ~/catkin_ws
-source /opt/ros/melodic/setup.bash
+source ~/.bashrc
+
+cd ~/mrs_workspace
 
 catkin run_tests mrs_uav_testing
 
-catkin_test_results ~/catkin_ws/build/mrs_uav_testing
+catkin_test_results ~/mrs_workspace/build/mrs_uav_testing
