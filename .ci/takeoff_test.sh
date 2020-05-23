5fec005bbe1f5588b35cfd7e4e155ab9fa0726f1
Tomas Baca <klaxalk@gmail.com>
2020-05-23 23:09:53 +0200

    takeoff test sourcing fix

--- .ci/takeoff_test.sh
+++ .ci/takeoff_test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 echo "Starting takeoff test" 
-source ~/.bashrc
+
+source /opt/ros/melodic/setup.bash
+source ~/mrs_workspace/devel/setup.bash
+
+export UAV_NAME=uav1
+export RUN_TYPE="simulation" # {simulation, uav}
+export UAV_TYPE="t650" # {f550, f450, t650, eagle, naki}
+export PROPULSION_TYPE="default" # {default, new_esc, ...}
+export ODOMETRY_TYPE="gps" # {gps, optflow, hector, vio, ...}
 
 cd ~/mrs_workspace
 
