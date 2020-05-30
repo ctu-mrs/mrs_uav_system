d7b168806a696470d71ddff30bea1142a10752dc
Tomas Baca <klaxalk@gmail.com>
2020-05-30 23:35:07 +0200

    added missing mrs_workspace extend

--- scripts/set_my_workspace.sh
+++ scripts/set_my_workspace.sh
@@ -24,6 +24,8 @@ command catkin config --profile debug --cmake-args -DCMAKE_EXPORT_COMPILE_COMMAN
 command catkin config --profile release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17 -march=native -fno-diagnostics-color'  -DCMAKE_C_FLAGS='-march=native -fno-diagnostics-color'
 command catkin config --profile reldeb --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17 -march=native -fno-diagnostics-color' -DCMAKE_C_FLAGS='-march=native -fno-diagnostics-color'
 
+command catkin config --extend $MRS_WORKSPACE/devel
+
 # normal installation
 [ -z "$TRAVIS_CI" ] && command catkin profile set reldeb
 
