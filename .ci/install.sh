0bae07d9d675eb9310aa36935ae01e6bc61237c8
Tomas Baca <klaxalk@gmail.com>
2020-11-28 16:05:28 +0100

    travis -> actions

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,14 +2,12 @@
 # author: Robert Penicka
 set -e
 
-echo "Starting install preparation" 
-openssl aes-256-cbc -K $encrypted_f0fd3ee254e8_key -iv $encrypted_f0fd3ee254e8_iv -in ./.ci/deploy_key_github.enc -out ./.ci/deploy_key_github -d
-eval "$(ssh-agent -s)"
-chmod 600 ./.ci/deploy_key_github
-ssh-add ./.ci/deploy_key_github
-
 sudo apt-get -y update -qq
 sudo apt-mark hold openssh-server
+
+# 20.04 problem fix
+sudo apt-get -y install grub-efi
+sudo update-grub
 
 # the "gce-compute-image-packages" package often freezes the installation at some point
 # the installation freezes when it tries to manage some systemd services
