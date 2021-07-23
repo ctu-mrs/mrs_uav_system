aafcd72e118c523538bd8ff0e135628e8cb7f0fa
Tomas Baca <klaxalk@gmail.com>
2021-07-23 08:11:26 +0200

    fixed gitman installation

--- dependencies/gitman.sh
+++ dependencies/gitman.sh
@@ -10,10 +10,17 @@ echo "$0: installing Gitman"
 distro=`lsb_release -r | awk '{ print $2 }'`
 
 if [ "$distro" = "18.04" ]; then
-  sudo apt-get -y install python-pip python3-pip python-setuptools python3-setuptools
-elif [ "$distro" = "20.04" ]; then
-  sudo apt-get -y install python3-pip python3-setuptools
-fi
 
-sudo pip3 install gitman==2.3.0
-sudo -H pip3 install gitman==2.3.0
+  sudo apt-get -y install python-pip python3-pip python-setuptools python3-setuptools
+
+  sudo pip3 install gitman
+  sudo -H pip3 install gitman
+
+elif [ "$distro" = "20.04" ]; then
+
+  sudo apt-get -y install python3-pip python3-setuptools
+
+  sudo pip3 install gitman==2.3.0
+  sudo -H pip3 install gitman==2.3.0
+
+fi
