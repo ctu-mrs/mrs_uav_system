c2448b0ecbff0d805fab60ea19db271026566fe4
Tomas Baca <klaxalk@gmail.com>
2022-01-13 10:33:01 +0100

    split docker with/without linux setup

---
+++ .ci/docker_with_linux_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+docker build . --file docker/with_linux_setup --tag ctumrs/mrs_uav_system_ls:latest
+
+docker login --username klaxalk --password $TOKEN
+
+docker push ctumrs/mrs_uav_system_ls:latest
--- .ci/docker_without_linux_setup.sh
+++ .ci/docker_without_linux_setup.sh
@@ -5,7 +5,7 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
-docker build . --file Dockerfile --tag ctumrs/mrs_uav_system:latest
+docker build . --file docker/without_linux_setup --tag ctumrs/mrs_uav_system:latest
 
 docker login --username klaxalk --password $TOKEN
 
