a27639a2e59be95d01f1c7ea2c5cfb303c2a5696
Tomas Baca <klaxalk@gmail.com>
2022-02-14 13:18:08 +0100

    added singularity action

---
+++ .ci/singularity.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+# get the path to the repository
+THIS_PATH=`dirname "$0"`
+THIS_PATH=`( cd "$THIS_PATH" && pwd )`
+
+echo $TOKEN >> /tmp/token
+
+git clone https://github.com/ctu-mrs/mrs_singularity
+cd $THIS_PATH/mrs_singularity/install
+./install_singularity.sh
+cd $THIS_PATH/mrs_singularity/recipes/04_with_linux_setup_uav_modules
+
+echo ""
+echo "Building Singularity image"
+
+./build.sh
+
+singularity remote login --tokenfile /tmp/token
+
+singularity push -U 
