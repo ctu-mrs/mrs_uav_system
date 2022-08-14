2aff6e031253b5905a78b4e113e7a01b5c77abb5
Tomas Baca <klaxalk@gmail.com>
2022-08-14 22:00:27 +0200

    updated ros installation

--- dependencies/ros.sh
+++ dependencies/ros.sh
@@ -19,7 +19,7 @@ sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `ls
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 sudo apt-get -y update
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $ROS_DISTRO main" > /etc/apt/sources.list.d/ros-latest.list'
 
 sudo apt-get -y install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
