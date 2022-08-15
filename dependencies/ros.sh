66e7895f3f8813bf0d5fde368561781f4d8f6920
Tomas Baca <klaxalk@gmail.com>
2022-08-15 08:04:37 +0200

    wip

--- dependencies/ros.sh
+++ dependencies/ros.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+sudo apt-get -y install wget lsb-release gnupg curl
+
 distro=`lsb_release -r | awk '{ print $2 }'`
 [ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
 [ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
@@ -9,14 +14,14 @@ distro=`lsb_release -r | awk '{ print $2 }'`
 debian=`lsb_release -d | grep -i debian | wc -l`
 [[ "$debian" -eq "1" ]] && ROS_DISTRO="noetic"
 
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
-
 echo "$0: Installing ROS"
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-sudo apt-get -y install curl
+# add repository for ignition library
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo apt-get -y update
