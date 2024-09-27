bcb96fae540ec45489ca75a467af789e9d351170
Tomas Baca <klaxalk@gmail.com>
2024-09-27 15:20:41 +0200

    wip old system docker

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,10 @@ MY_PATH=`( cd "$MY_PATH" && pwd )`
 
 cd $MY_PATH
 
-docker build -f ./1.5 -t ctumrs/mrs_uav_system:1.5 .
+LOCAL_TAG=mrs_uav_system:b3m33mrs
+REGISTRY=ctumrs
+
+docker buildx create --name container --driver=docker-container
+docker buildx build . --file ./old_system -t "$REGISTRY/$LOCAL_TAG" --platform=linux/arm64 --progress=plain
+
+docker push "$REGISTRY/$LOCAL_TAG"
